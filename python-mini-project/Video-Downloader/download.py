fe5f2bccd8d8bb96ba31e45c874c009eef904d9d
Nhan Phan <32973466+Enzophan@users.noreply.github.com>
2022-10-07 12:21:46 +0700

    Add files via upload

---
+++ python-mini-project/Video-Downloader/download.py
@@ -0,0 +1,14 @@
+from pytube import YouTube
+
+def Download(link):
+    youtubeObject = YouTube(link)
+    youtubeObject = youtubeObject.streams.get_highest_resolution()
+    try:
+        youtubeObject.download()
+    except:
+        print("Some Errors Occurred")
+
+    print("Done!")
+
+link = input("YouTube Video URL: ")
+Download(link)
