cdc0784106034e1c15ee67f3adf886feb67c59f6
Nhan Phan <32973466+Enzophan@users.noreply.github.com>
2023-03-21 18:08:56 +0700

    Add files via upload

--- python-mini-project/Jira-dashboard/main.py
+++ python-mini-project/Jira-dashboard/main.py
@@ -4,18 +4,27 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
+
 df = pd.read_csv('Jira-dashboard/Jira.csv')
 
 df.set_index('Issue key', inplace=True)
 
-df = df[['Summary', 'Labels', 'Priority', 'Sprint', 'Reporter', 'Creator', 'Issue Type', 'Status', 'Updated']]
+df = df[['Summary', 'Labels', 'Priority', 'Sprint', 'Reporter', 'Creator', 'Issue Type', 'Status', 'Custom field (Story Points)', 'Updated']]
 df['Updated'] = pd.to_datetime(df['Updated'])
 df['Date'] = df['Updated'].dt.date
 
 # Replace text
 df['Labels'] = df['Labels'].fillna('None')
 
-print(df.head())
+# print(df.head())
 # print(df.count())
-print(sum(df['Labels'] == 'Pricer_Quotation'))
-print(df.groupby(['Labels']).size())
+# print(sum(df['Labels'] == 'Pricer_Quotation'))
+# print(df.groupby(['Labels']).size())
+# print(df.groupby(['Sprint']).size())
+
+point = sum(df['Custom field (Story Points)'] != '')
+print(point)
+
+print(df.groupby(['Issue Type']).size().reset_index(name='counts'))
+print(df.groupby(['Issue Type']).apply(lambda x: x['Issue Type'].count()/point).reset_index(name='average'))
+
